6c7d590f1dc627bf61058e3614e38576d2f16740
hasemizu0507 <hasemizu0507@ezweb.ne.jp>
2022-01-05 14:49:53 +0900

    "develop"ブランチを修正

--- winedeliveryservice_private/winedelivery/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-
-# モデルをインポート 
-from .models import Profile, RentRoom, Cart
-
-# 管理サイトへのモデルの登録 
-admin.site.register(Profile) 
-admin.site.register(RentRoom) 
-admin.site.register(Cart) 
--- winedeliveryservice_private/winedelivery/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class winedeliveryConfig(AppConfig):
-    name = 'winedelivery'
